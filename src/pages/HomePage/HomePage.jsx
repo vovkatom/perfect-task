f6d85a10e6e86195be5282b01a1fe49b9fbed63a
sunny7657 <ksvetlana466@gmail.com>
2024-05-04 00:52:38 +0200

    create home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,29 @@
+import Container from '../../components/Container/Container';
+import Header from '../../components/Header/Header';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import ScreensPage from '../ScreensPage/ScreensPage';
+import css from './HomePage.module.css';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <div className={css.background}>
+        <Container className="home-page">
+          <ScreensPage />
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
+
+//"HomePage - складається з компонентів Header, Sidebar, ScreensPage.
+
+// Header - це шапка HomePage.
+
+// Sidebar - компонент в якому знаходиться навігація по дошкам, та можливість їх створювати.
+
+// ScreensPage - компонент в якому знаходиться основний контент дошки(колонки та картки). Перехід на цей компонент відбувається за динамічним роутом /home/:boardName."
