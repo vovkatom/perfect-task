796c9c91657ec918ee775866343c7389d0f82f46
Vladimir Bohachuk <36505066+vovkatom@users.noreply.github.com>
2024-05-03 03:02:24 +0300

    Revert "fix vite"

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/perfect-task">
+    <BrowserRouter basename="/Perfect-Task">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
           assets: '/src/assets'
         },
       },
-      base: '/perfect-task/',
+      base: '/Perfect-Task/',
     });
