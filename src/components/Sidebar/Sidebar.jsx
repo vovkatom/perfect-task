3d233308d634b1db008a3bf337ff31aabc61714b
sunny7657 <ksvetlana466@gmail.com>
2024-05-04 00:58:08 +0200

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,15 @@
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import css from '../SideBar/SideBar.module.css';
+import Logo from './Logo/Logo';
+import CreateNewBoard from './CreateNewBoard/CreateNewBoard';
+
+const SideBar = () => {
+  return (
+    <div className={css.container}>
+      <Logo />
+      <h3 className={css.myBoardsTitle}>My boards</h3>
+      <CreateNewBoard />
+    </div>
+  );
 };
 
-export default Sidebar;
+export default SideBar;
