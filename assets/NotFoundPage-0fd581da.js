586d566b1d34d73232404cc822cb2a7d86b8b5e2
vovkatom <36505066+vovkatom@users.noreply.github.com>
2024-05-03 21:42:55 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@b40e2f92fde039542988092177fdac013365337b 🚀

---
+++ assets/NotFoundPage-0fd581da.js
@@ -0,0 +1,1 @@
+import{j as o,L as e}from"./index-ea2c17f6.js";const s=()=>o.jsxs("div",{children:[o.jsx("h1",{children:"Cannot found this page"}),o.jsx(e,{to:"/home",children:"To home page"})]});export{s as default};
---
+++ assets/ScreensPage-77eba9d0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-ea2c17f6.js";const s=()=>e.jsx("div",{children:"ScreensPage"});export{s as default};
