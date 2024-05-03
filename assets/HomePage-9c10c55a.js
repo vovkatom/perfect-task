b920054e1e7ae4a96b1484e6ba1fe3e4c1dbdbb9
vovkatom <36505066+vovkatom@users.noreply.github.com>
2024-05-03 09:43:39 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@1060a4236ea4febcf9973095d12290000daaace9 🚀

---
+++ assets/HomePage-9c10c55a.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-e06ab8a5.js";const r=()=>e.jsx("div",{children:"HomePage"});export{r as default};
---
+++ assets/ScreensPage-9c38ba06.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-e06ab8a5.js";const s=()=>e.jsx("div",{children:"ScreensPage"});export{s as default};
