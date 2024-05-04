cd6b192a94d2dd30d32992ecb0a558da081fa4ef
sunny7657 <ksvetlana466@gmail.com>
2024-05-04 20:37:30 +0200

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import Container from '../../components/Container/Container';
 import Header from '../../components/Header/Header';
-import SideBar from '../../components/Sidebar/Sidebar';
+import SideBar from '../../components/Sidebar/SideBar';
 import ScreensPage from '../ScreensPage/ScreensPage';
 import css from './HomePage.module.css';
 
