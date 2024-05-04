f8e189648c5c60e31cc4b185acc7eb8e3893a3ef
Vladimir Bohachuk <36505066+vovkatom@users.noreply.github.com>
2024-05-04 22:25:48 +0300

    Revert "Feature/home page"

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,12 @@
-import Container from '../../components/Container/Container';
-import Header from '../../components/Header/Header';
-import SideBar from '../../components/Sidebar/SideBar';
-import ScreensPage from '../ScreensPage/ScreensPage';
-import css from './HomePage.module.css';
+import SideBar from '../../components/SideBar/SideBar';
 
 const HomePage = () => {
   return (
-    <div className={css.background}>
-      <Header />
+    <div>
+      HomePage
       <SideBar />
-      <Container className="home-page">
-        <ScreensPage />
-      </Container>
     </div>
   );
 };
 
 export default HomePage;
-
-//"HomePage - складається з компонентів Header, Sidebar, ScreensPage.
-
-// Header - це шапка HomePage.
-
-// Sidebar - компонент в якому знаходиться навігація по дошкам, та можливість їх створювати.
-
-// ScreensPage - компонент в якому знаходиться основний контент дошки(колонки та картки). Перехід на цей компонент відбувається за динамічним роутом /home/:boardName."
