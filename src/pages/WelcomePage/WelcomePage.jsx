10956700236fbd1850d2b4e1a1e3e91e367b4d76
sunny7657 <ksvetlana466@gmail.com>
2024-05-05 22:23:37 +0200

    Update WelcomePage.jsx

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -12,17 +12,17 @@ const WelcomePage = () => {
             <img
               className={css.logoAvatar}
               srcSet="
-              assets/img/start-avatar/start-user-124.png 124w,
-              assets/img/start-avatar/start-user_desktop-162.png 162w,
-              assets/img/start-avatar/start-user@2x-248.png 248w,
-              assets/img/start-avatar/start-user_desktop@2x-324.png 324w,
+              /assets/img/start-avatar/start-user-124.png 124w,
+              /assets/img/start-avatar/start-user_desktop-162.png 162w,
+              /assets/img/start-avatar/start-user@2x-248.png 248w,
+              /assets/img/start-avatar/start-user_desktop@2x-324.png 324w,
               "
               sizes="
               (min-width: 1200px) 162px,
               (min-width: 768px) 162px,
               (min-width: 375px) 124px
               "
-              src="assets/img/start-avatar/start-user-124.png"
+              src="/assets/img/start-avatar/start-user-124.png"
               alt="Task Pro avatar"
               width="124"
               height="124"
