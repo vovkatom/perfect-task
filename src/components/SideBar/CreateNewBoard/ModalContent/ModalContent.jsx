e224c67ca0f6bfaf4f98599b360412dc6156cb2c
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-05-05 23:16:06 +0200

    modal content

---
+++ src/components/SideBar/CreateNewBoard/ModalContent/ModalContent.jsx
@@ -0,0 +1,23 @@
+import BackgroundSet from './BackgroundSet/BackgroundSet';
+import ButtonCreateBoard from './ButtonCreateBoard/ButtonCreateBoard';
+import IconsSet from './IconsSet/IconsSet';
+import css from '../ModalContent/ModalContent.module.css';
+
+const ModalContent = () => {
+  return (
+    <>
+      <svg width="18" height="18">
+        icon close
+      </svg>
+      <p className={css.newBoard}>New board</p>
+      <input className={css.inputTitle} placeholder="Title"></input>
+      <p>Icons</p>
+      <IconsSet />
+      <p>Background</p>
+      <BackgroundSet />
+      <ButtonCreateBoard />
+    </>
+  );
+};
+
+export default ModalContent;
