be2c0cac016691c95b0efb992d62a0e07668523f
sunny7657 <129301566+sunny7657@users.noreply.github.com>
2024-05-05 22:12:07 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@8a0da9f78e67c1d026379307eb67723eff410a26 🚀

--- assets/ScreensPage-6312a91c.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-4e1c4731.js";const s=()=>e.jsx("div",{children:"ScreensPage"});export{s as default};
---
+++ assets/ScreensPage-e9af8a79.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-16f8084a.js";const s=()=>e.jsx("div",{children:"ScreensPage"});export{s as default};
