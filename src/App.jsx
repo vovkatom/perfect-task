e1bc24d0f289b4d299835a8e01bd92792e83d652
Vladimir Bogachuk <vovkatom@gmail.com>
2024-05-07 17:07:05 +0300

    dell approutes insert app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import AppRoutes from './AppRoutes';
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+import SharedLayout from './components/SharedLayout/SharedLayout';
+import AuthPage from './pages/AuthPage/AuthPage';
 
-const App = () => {
+// import PublicRoute from './components/PublicRoute/PublicRoute';
+// import PrivateRoute from './components/PrivateRoute/PrivatRoute';
+
+const WelcomePage = lazy(() => import('./pages/WelcomePage/WelcomePage'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const ScreensPage = lazy(() => import('./pages/ScreensPage/ScreensPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+
+const AppRoutes = () => {
   return (
-    <>
-      <AppRoutes />
-    </>
+    <Routes>
+      <Route path='/' element={<SharedLayout />}>
+        {/* <Route element={<PublicRoute />}> */}
+          <Route index element={<WelcomePage />} />
+        {/* </Route> */}
+        <Route path='auth/:id' element={<AuthPage />} />
+        {/* <Route element={<PrivateRoute />}> */}
+          <Route path='home' element={<HomePage />} />
+        {/* </Route> */}
+        <Route path='home/:boardName' element={<ScreensPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 };
-export default App;
+
+export default AppRoutes;
