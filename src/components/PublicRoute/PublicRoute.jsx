eef03b43710c2f2b0610403cb14f19d5b0b9ee5b
Lena <elenazyamileva@gmail.com>
2024-05-08 19:19:52 +0300

    commit

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,11 +1,14 @@
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
-import { selectIsLogin, selectToken } from '../../redux/auth/auth-selectors';
+import {
+  selectIsLogin,
+  selectAccessToken,
+} from '../../redux/auth/auth-selectors';
 import Loader from '../Loader/Loader';
 
 const PublicRoute = () => {
   const isLogin = useSelector(selectIsLogin);
-  const token = useSelector(selectToken);
+  const token = useSelector(selectAccessToken);
 
   if (!isLogin && token) {
     return <Loader />;
