356f74e0978491e13bb3fb124eff75513174295c
sunny7657 <ksvetlana466@gmail.com>
2024-05-08 14:39:01 +0200

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 import AuthPage from './pages/AuthPage/AuthPage';
 import { lazy } from 'react';
 
 import PublicRoute from './components/PublicRoute/PublicRoute';
 import PrivateRoute from './components/PrivateRoute/PrivatRoute';
+import { useSelector } from 'react-redux';
+import { selectIsLogin } from './redux/auth/auth-selectors';
 
 const WelcomePage = lazy(() => import('./pages/WelcomePage/WelcomePage'));
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
@@ -12,9 +14,16 @@ const ScreensPage = lazy(() => import('./pages/ScreensPage/ScreensPage'));
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 const AppRoutes = () => {
+  const isLogin = useSelector(selectIsLogin);
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
+        <Route
+          index
+          element={
+            isLogin ? <Navigate to="/home" /> : <Navigate to="/welcome" />
+          }
+        />
         <Route path="welcome" element={<WelcomePage />} />
         <Route element={<PublicRoute />}>
           <Route path="auth/:id" element={<AuthPage />} />
