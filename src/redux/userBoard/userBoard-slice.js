5539662cf45786ff7c09a1f099b5ab73585a6760
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-05-09 23:22:43 +0200

    redux - boards

---
+++ src/redux/userBoard/userBoard-slice.js
@@ -0,0 +1,59 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { pending, rejected } from '../../shared/redux/redux';
+import {
+  getBoardByID,
+  deleteBoardByID,
+  addBoard,
+  updateBoard,
+  getAllBoards,
+} from '../userBoard/userBoard-operations';
+
+const initialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
+
+const boardSlice = createSlice({
+  name: 'boards',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(addBoard.pending, pending)
+      .addCase(addBoard.fulfilled, (state, { payload }) => ({
+        ...state,
+        isLoading: false,
+        items: [...state.items, payload],
+      }))
+      .addCase(getAllBoards.pending, pending)
+      .addCase(getAllBoards.fulfilled, (state, { payload }) => ({
+        ...state,
+        isLoading: false,
+        items: payload,
+      }))
+      .addCase(getAllBoards.rejected.rejected)
+
+      .addCase(getBoardByID.pending, pending)
+      .addCase(getBoardByID.fulfilled, (state, { payload }) => ({
+        ...state,
+        isLoading: false,
+        items: payload,
+      }))
+      .addCase(getBoardByID.rejected, rejected)
+
+      .addCase(deleteBoardByID.pending, pending)
+      .addCase(deleteBoardByID.fulfilled, (state, { payload }) => ({
+        ...state,
+        isLoading: false,
+        items: state.items.filter((item) => item.id !== payload),
+      }))
+      .addCase(deleteBoardByID.rejected, rejected)
+
+      .addCase(updateBoard.pending, pending)
+      .addCase(updateBoard.fulfilled, (state, { payload }) => ({
+        ...state,
+        isLoading: false,
+        items: state.items.filter((item) => item.id === payload),
+      }));
+  },
+});
