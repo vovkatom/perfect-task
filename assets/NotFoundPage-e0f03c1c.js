32a47f4af1691f096ded8a8053e6f8c8ae8770f8
vovkatom <36505066+vovkatom@users.noreply.github.com>
2024-05-10 20:48:12 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@4059059d4ca0a5ba48af9f28d3f0706aad742a89 🚀

---
+++ assets/NotFoundPage-e0f03c1c.js
@@ -0,0 +1,1 @@
+import{j as o,L as e}from"./index-d8a81a61.js";const s=()=>o.jsxs("div",{children:[o.jsx("h1",{children:"Cannot found this page"}),o.jsx(e,{to:"/home",children:"To home page"})]});export{s as default};
