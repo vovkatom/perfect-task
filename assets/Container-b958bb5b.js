7902c8b5e91e4427c385296ce17594c9de6b08de
vovkatom <36505066+vovkatom@users.noreply.github.com>
2024-05-11 23:46:18 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@3e026b1ed9dfa8d33fd4411228af00c126dc7c81 🚀

--- assets/Container-b958bb5b.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-ff678a03.js";const o="_container_a3bby_1",r={container:o},e=({className:s,children:t})=>n.jsx("section",{className:s,children:n.jsx("div",{className:r.container,children:t})});export{e as C};
