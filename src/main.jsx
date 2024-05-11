5d8fc62c9a27984957de2af725a7dd30d26cc97e
sunny7657 <ksvetlana466@gmail.com>
2024-05-12 00:56:56 +0200

    comment reload

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import './js/notiflix.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/store';
-import './js/notiflix.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
