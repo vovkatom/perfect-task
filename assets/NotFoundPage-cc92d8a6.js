7fd2ceae6247fef9a3b675d496cd6042d7aaccb0
sunny7657 <129301566+sunny7657@users.noreply.github.com>
2024-05-11 20:57:04 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@68ff71339e345ac1c07594faa71b80efc2b60c11 🚀

--- assets/NotFoundPage-cc92d8a6.js
+++
@@ -1,1 +0,0 @@
-import{j as o,L as e}from"./index-bad7cb70.js";const s=()=>o.jsxs("div",{children:[o.jsx("h1",{children:"Cannot found this page"}),o.jsx(e,{to:"/home",children:"To home page"})]});export{s as default};
