be7722663be9ed78bb8a611c5817e7df73ad0d9d
sunny7657 <129301566+sunny7657@users.noreply.github.com>
2024-05-12 15:38:29 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@97ee82f8a7f96d0fcb5732daea8ae4f4e68de397 🚀

--- assets/Icon-ad03354b.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-611124b2.js";const a="/perfect-task/assets/icon-a9a39900.svg",c=({id:e,className:r,width:t,height:n})=>s.jsx(s.Fragment,{children:s.jsx("svg",{className:r,width:t,height:n,children:s.jsx("use",{href:`${a}#${e}`})})});export{c as I,a as s};
