923d080644fee824cb5ec7e92e9f8d1657542122
Andrew Butenko <sedkol88@gmail.com>
2024-05-13 01:04:47 +0300

    Loader for NeedHelpModal
    Добавлен лоадер для модалки NeedHelpModal

--- src/components/SideBar/NeedHelp/NeedHelpModal/NeedHelpModal.jsx
+++ src/components/SideBar/NeedHelp/NeedHelpModal/NeedHelpModal.jsx
@@ -1,8 +1,10 @@
 import { useState, useId } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { support } from '../../../../redux/user/user-operations';
+import Loader from '../../../../components/Loader/Loader';
 import css from './need-help-modal.module.css';
 import clsx from 'clsx';
+import { selectAuthLoading } from '../../../../redux/auth/auth-selectors.js';
 
 const INITIAL_STATE = {
   email: '',
@@ -12,6 +14,7 @@ const INITIAL_STATE = {
 const NeedHelpModal = () => {
   const [formData, setFormData] = useState({ ...INITIAL_STATE });
   const dispatch = useDispatch();
+  const loading = useSelector(selectAuthLoading);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -71,7 +74,7 @@ const NeedHelpModal = () => {
           />
         </div>
         <button className={buttonClassName} type="submit">
-          Send
+          {loading ? <Loader /> : 'Send'}
         </button>
       </form>
     </div>
