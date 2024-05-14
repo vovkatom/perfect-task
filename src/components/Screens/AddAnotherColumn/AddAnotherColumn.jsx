ab56e4063286b9cd7d3fe08cb0d6c150ca15b443
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-05-14 13:17:55 +0200

    add Button component

---
+++ src/components/Screens/AddAnotherColumn/AddAnotherColumn.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+//import css from './AddAnotherColumn.module.css';
+import CommonModal from '../../CommonModal/CommonModal';
+import ModalContent from './ModalContent/ModalContent';
+import Button from '../../../shared/Button/Button';
+
+const AddAnotherColumn = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Button
+        id="icon-plus"
+        type="button"
+        onClick={openModal}
+        title="Add another column"
+      />
+      <CommonModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        title="Add column"
+      >
+        <ModalContent closeModal={handleCloseModal} />
+      </CommonModal>
+    </>
+  );
+};
+
+export default AddAnotherColumn;
