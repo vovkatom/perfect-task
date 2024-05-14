d06ca5c61df54fa87a61f70668bbfff2563b1772
Vladimir Bogachuk <vovkatom@gmail.com>
2024-05-15 01:03:57 +0300

    board

--- src/components/SideBar/CreateNewBoard/CreateBoardForm/BackgroundSet/BackgroundSet.jsx
+++ src/components/SideBar/CreateNewBoard/CreateBoardForm/BackgroundSet/BackgroundSet.jsx
@@ -4,15 +4,11 @@ import css from '../BackgroundSet/BackgroundSet.module.css';
 const BackgroundSet = ({ bgImages, updateBg }) => {
   const [selectedBg, setSelectedBg] = useState(null);
 
-  //console.log(Object.keys(bgImages));
-
-  const handleBgClick = (selectedBg) => {
+  const handleBgClick = selectedBg => {
     setSelectedBg(selectedBg);
     console.log(selectedBg);
     updateBg(selectedBg);
   };
-
-  //console.log(Object.entries(bgImages));
 
   const imagesBG = Object.entries(bgImages).map(([key, value]) => (
     <li key={key} className={css.bgItem} onClick={() => handleBgClick(key)}>
