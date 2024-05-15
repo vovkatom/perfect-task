8ff4571becd64f4f7903e0640cf73bd6e249cdaa
sunny7657 <129301566+sunny7657@users.noreply.github.com>
2024-05-15 15:23:30 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@a099c5221d27bf4129bc25f17e4aa6633584171f 🚀

---
+++ assets/AuthPage-2666448d.js
@@ -0,0 +1,1 @@
+import{u as o,s as e,j as s,L as a}from"./index-5bbef0c3.js";import{A as i}from"./Auth-6d2dd1fb.js";import{s as n}from"./userBoard-selectors-e6afb3b7.js";import"./InputError-cde4ca95.js";const d=()=>{const t=o(n),r=o(e);return s.jsxs(s.Fragment,{children:[t&&s.jsx(a,{}),s.jsx(i,{}),r&&s.jsx("p",{style:{color:"red"},children:r})]})};export{d as default};
---
+++ assets/NotFoundPage-2c216a80.js
@@ -0,0 +1,1 @@
+import{j as e,e as o}from"./index-5bbef0c3.js";const s=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Cannot found this page"}),e.jsx(o,{to:"/home",children:"To home page"})]});export{s as default};
