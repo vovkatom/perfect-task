dc5a97e2b3478412c26c3b553d31dbf8cc4e71b3
NataPas2021 <94239256+NataPas2021@users.noreply.github.com>
2024-05-15 10:18:13 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@3b613bd22883921ab7b080934fc8ddfc0c05a7ad 🚀

--- assets/AuthPage-3f3a33ec.js
+++
@@ -1,1 +0,0 @@
-import{u as o,s as e,j as s,L as a}from"./index-5e7564b2.js";import{A as i}from"./Auth-e41b4b6e.js";import{s as n}from"./userBoard-selectors-1e841f4d.js";import"./InputError-2eadead2.js";const d=()=>{const t=o(n),r=o(e);return s.jsxs(s.Fragment,{children:[t&&s.jsx(a,{}),s.jsx(i,{}),r&&s.jsx("p",{style:{color:"red"},children:r})]})};export{d as default};
---
+++ assets/AuthPage-c86a8481.js
@@ -0,0 +1,1 @@
+import{u as o,s as e,j as s,L as a}from"./index-7a6fc305.js";import{A as i}from"./Auth-e777a818.js";import{s as n}from"./userBoard-selectors-005cfa60.js";import"./InputError-0fa29a46.js";const d=()=>{const t=o(n),r=o(e);return s.jsxs(s.Fragment,{children:[t&&s.jsx(a,{}),s.jsx(i,{}),r&&s.jsx("p",{style:{color:"red"},children:r})]})};export{d as default};
