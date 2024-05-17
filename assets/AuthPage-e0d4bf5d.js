b68cea41aefd99816a6a655bf2d6ba531d984eaf
vovkatom <36505066+vovkatom@users.noreply.github.com>
2024-05-17 13:24:57 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@7e6a6baaad7529f5fbb7bafa8353e1345b64b776 🚀

---
+++ assets/AuthPage-e0d4bf5d.js
@@ -0,0 +1,1 @@
+import{u as o,s as e,j as s,L as a}from"./index-64b466a3.js";import{A as i}from"./Auth-ec0863f8.js";import{s as n}from"./userBoard-selectors-34f83361.js";import"./InputError-b6e5cdce.js";const d=()=>{const t=o(n),r=o(e);return s.jsxs(s.Fragment,{children:[t&&s.jsx(a,{}),s.jsx(i,{}),r&&s.jsx("p",{style:{color:"red"},children:r})]})};export{d as default};
