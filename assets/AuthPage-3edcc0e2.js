b5648a962d94489378877d7c274ef73d0e7f1b53
sunny7657 <129301566+sunny7657@users.noreply.github.com>
2024-05-27 21:29:15 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@78400af753531eec1865ce2e8ba4c979a4802c4e 🚀

---
+++ assets/AuthPage-3edcc0e2.js
@@ -0,0 +1,1 @@
+import{u as o,s as e,j as s,L as a}from"./index-50e821c6.js";import{A as i}from"./Auth-c8e94eda.js";import{s as n}from"./userBoard-selectors-c3534e8d.js";import"./InputError-341e90b4.js";const d=()=>{const t=o(n),r=o(e);return s.jsxs(s.Fragment,{children:[t&&s.jsx(a,{}),s.jsx(i,{}),r&&s.jsx("p",{style:{color:"red"},children:r})]})};export{d as default};
