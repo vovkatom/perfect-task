1ff2cb2dacd529c5a614b0bda2b5655385c16856
Vladimir Bohachuk <36505066+vovkatom@users.noreply.github.com>
2024-06-18 22:54:36 +0300

    Revert "test"

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,28 +1,21 @@
 import { ColorRing } from 'react-loader-spinner';
-import styles from './Loader.module.css';
+import styles from './Loader.module.css'; 
 
 const Loader = ({ centered }) => {
-  const spinnerClass = centered
-    ? `${styles.spinner} ${styles.centered}`
-    : styles.spinner;
-  const spinnerSize = centered ? 64 : 32;
+  const spinnerClass = centered ? `${styles.spinner} ${styles.centered}` : styles.spinner;
+  const spinnerSize = centered ? 64 : 32; 
 
   return (
-    <div>
-      <p className={styles.render}>
-        Render....
-        <div className={spinnerClass}>
-          <ColorRing
-            visible={true}
-            height={spinnerSize}
-            width={spinnerSize}
-            ariaLabel='blocks-loading'
-            wrapperStyle={{}}
-            wrapperClass='blocks-wrapper'
-            colors={['#8FA1D0', '#E09CB5', '#f8b26a', '#abbd81', '#849b87']}
-          />
-        </div>
-      </p>
+    <div className={spinnerClass}>
+      <ColorRing
+        visible={true}
+        height={spinnerSize}
+        width={spinnerSize}
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={['#8FA1D0', '#E09CB5', '#f8b26a', '#abbd81', '#849b87']}
+      />
     </div>
   );
 };
