80f999c9952442fc40bbaeb04a838f6dfc6512c8
vovkatom <36505066+vovkatom@users.noreply.github.com>
2024-07-06 02:40:34 +0000

    Deploying to gh-pages from @ vovkatom/perfect-task@99e083bd6bdb50a682a296905c6b3d6b6bc24bdb 🚀

--- assets/AuthPage-18062c00.js
+++
@@ -1,1 +0,0 @@
-import{u as o,s as e,j as s,L as a}from"./index-6e3b3954.js";import{A as i}from"./Auth-f7ca8afd.js";import{s as n}from"./userBoard-selectors-7e8d9f8a.js";import"./InputError-54c85ac2.js";const d=()=>{const t=o(n),r=o(e);return s.jsxs(s.Fragment,{children:[t&&s.jsx(a,{}),s.jsx(i,{}),r&&s.jsx("p",{style:{color:"red"},children:r})]})};export{d as default};
---
+++ assets/AuthPage-2287e1ff.js
@@ -0,0 +1,1 @@
+import{u as o,s as e,j as s,L as a}from"./index-ed0ef6ed.js";import{A as i}from"./Auth-cc41d391.js";import{s as n}from"./userBoard-selectors-b92ffb63.js";import"./InputError-7afc3615.js";const d=()=>{const t=o(n),r=o(e);return s.jsxs(s.Fragment,{children:[t&&s.jsx(a,{}),s.jsx(i,{}),r&&s.jsx("p",{style:{color:"red"},children:r})]})};export{d as default};
